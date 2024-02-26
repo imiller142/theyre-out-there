25294d8fff29b56ba9dfadc8a9a46098885838f6
Ian Miller <imiller142@gmail.com>
2024-02-26 11:10:33 -0600

    created post for fish caught that also will create a fish>lake association

--- controller/api/catch-routes.js
+++ controller/api/catch-routes.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
 });
 
 
+
 router.put('/', (req, res) => {
     // make sure the session exists first
     if (req.session) {
@@ -27,6 +28,26 @@ router.put('/', (req, res) => {
     }
   });
   
+//this will create a new catch linked to a user and create all links between data basses if needed for new lakes and new fish
+// TODO: auth
+
+router.post('/', (req,res) => {
+  Fish_Caught.create({
+    species : req.body.species,
+    length: req.body.length,
+    weight: req.body.weight,
+    //Todo update to session auth
+    user_id: req.body.user_id,
+    lake_id: req.body.lake_id,
+    fish_id: req.body.fish_id
+  })
+  .then(Fish_Caught.ascFishLake({...req.body}, {Fish_db, Fish_Available, Lakes }))
+  .then(updatedFishData => res.json(updatedFishData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+})
 
 
 
