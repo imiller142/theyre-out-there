94321a21d1405e2b8af34df7fb83da8e62aa3641
Ian Miller <imiller142@gmail.com>
2024-02-26 17:26:41 -0600

    starting views

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.engine('handlebars', hbs.engine);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
 app.use(session(sess))
 
 // turn on routes
