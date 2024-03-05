bec37700379c82815a5df1d8d23c4a86b70c1e6a
Ian Miller <imiller142@gmail.com>
2024-03-05 16:39:34 -0600

    Add fish page being created

---
+++ controller/api/fish-available-routes.js
@@ -0,0 +1,12 @@
+﻿const router = require('express').Router();
+const { Lakes, Fish_Available, Fish_Caught, Fish_db } = require('../../model');
+
+router.get('/', (req, res) => {
+    Fish_Available.findAll({
+    })
+    .then(dbLakeData => res.json(dbLakeData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
---
+++ src/js/addFish.js
@@ -0,0 +1,35 @@
+﻿async function addFake(event) {
+    event.preventDefault()
+    const name = document.querySelector('.lake-name').value.trim();
+    const city = document.querySelector('.city').value.trim();
+    const latitude = document.querySelector('.latitude').value.trim();
+
+
+    if (latitude >  90 || latitude < -90) {
+      alert('Invalid Latitude')
+    }
+    else if (longitude > 180 || longitude < -180) {
+      alert('invalid longitude')
+    }
+    else{
+      if (name && city && latitude && longitude) {
+        const response = await fetch('/api/fish', {
+            method: 'post',
+            body: JSON.stringify({
+              description,
+              common_name,
+              wiki_link
+            }),
+            headers: { 'Content-Type': 'application/json' }
+          });
+
+        if (response.ok) {
+          document.location.replace('/lakes');
+        } else {
+          alert(response.statusText);
+        }
+      }
+  }
+}
+
+document.querySelector('#add-lake').addEventListener('submit', addLake);
